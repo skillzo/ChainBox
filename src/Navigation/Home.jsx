30a39b2cb39cb1ccb16b9c1aeedba6c2eff479cb
earlpappi <earlpappi@gmail.com>
2023-02-27 08:28:21 +0100

    --stash: stashinggg

---
+++ src/Navigation/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Profile from "../Components/Profile/Profile";
+import Footer from "../Components/Footer/Footer";
+import "./navigation.css";
+import Navbar from "../Components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Wrapper from "../Components/Wrapper/Wrapper";
+
+function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="navigation-header">
+          <Profile />
+          <Navbar />
+        </div>
+        <div className="navigation-body">
+          <Wrapper>
+            <Outlet />
+          </Wrapper>
+        </div>
+        <Footer />
+      </div>
+      {/* New comment */}
+      <div className="on-desktop">
+        This app is optimized for just mobile please view on a mobile device
+      </div>
+    </>
+  );
+}
+
+export default Home;
